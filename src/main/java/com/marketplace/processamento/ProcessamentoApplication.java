e2e61488f908046a85da0c8ab5675dd314598578
mateusrdg <mateusrodrigues-cp@hotmail.comgit config --global user.emailgit config --global user.email mateusrodrigues-cp@hotmail.com>
2020-05-04 12:50:11 -0300

    Servico processamento de venda

---
+++ src/main/java/com/marketplace/processamento/ProcessamentoApplication.java
@@ -0,0 +1,13 @@
+package com.marketplace.processamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcessamentoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProcessamentoApplication.class, args);
+    }
+
+}
